c2b2af33737c39885224c6cc344b20cd0184455e
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-10-02 01:14:43 +0900

    Fix: iOSでの高さ修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,35 @@
 <template>
-  <main class="bg-themeColor text-white w-screen h-screen flex flex-col gap-6 justify-center items-center">
+  <main class='bg-themeColor text-white w-screen flex flex-col gap-6 justify-center items-center height-setter'>
     <Countdown />
   </main>
 </template>
 
 <script>
 import Countdown from '@/components/Countdown.vue'
+
 export default {
   components: {
-    Countdown,
+    Countdown
   },
+  mounted() {
+    const setFillHeight = () => {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+
+    window.addEventListener('resize', setFillHeight)
+
+    setFillHeight()
+  }
 }
 </script>
-<style></style>
+<style>
+html {
+  @apply bg-themeColor
+}
+
+.height-setter {
+  min-height: 100vh;
+  min-height: calc(var(--vh, 1vh) * 100);
+}
+</style>
